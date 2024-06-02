d6911ca260355588b69ce59e9c09581850e8270c
tim <timothy.j.dailey@gmail.com>
2024-06-02 16:18:42 -0700

    cmake default - hello world

---
+++ astar-boost/astar-boost/astar-boost.cpp
@@ -0,0 +1,12 @@
+﻿// astar-boost.cpp : Defines the entry point for the application.
+//
+
+#include "astar-boost.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
